f8b9115fafb1c9dd29d185d36453915699a1402e
David <v.david7788@gmail.com>
2020-03-20 21:00:53 -0400

    maximum numbers

--- reduce_example.py
+++ reduce_example.py
@@ -20,8 +20,16 @@ list of numbers
 def lambda_test(functions,my_list):
     return reduce(functions, my_list)
 
+
+""" Determining the maximum of a list of numerical values """
+def maximum_number(numbers):
+    f = lambda x, y: x if (x > y) else y
+    return reduce(f, numbers)
+
 if __name__ == "__main__":
     sum_numbers = lambda x, y : x + y 
     list_number = [47, 11, 42, 13]
     result = lambda_test(sum_numbers,list_number)
     print(result)
+    
+    maximum_number(list_number)
